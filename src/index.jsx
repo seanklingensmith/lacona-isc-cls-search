2f115276d7c80050601864cd018441add727629f
seanklingensmith <klingensmithsd@gmail.com>
2016-10-14 22:23:34 -0400

    working prototype

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,48 @@
 /** @jsx createElement */
+
 import { createElement } from 'elliptical'
 import { Command } from 'lacona-phrases'
 import { runApplescript } from 'lacona-api'
+import { openURL } from 'lacona-api'
 
-export const MyNewCommand = {
-  extends: [Command],
+import _ from 'lodash'
+import classes from './classes'
 
-  execute (result) {
-    console.log('executing MyNewCommand')
-    runApplescript({script: `display alert "${result}"`})
-  },
+const ISCClass = {
+  describe() {
 
-  describe () {
+    let classlist = _.map(classes, (value) => {
+      return (<literal text={value} value={encodeURIComponent(value)}/>)
+    })
+
     return (
-      <literal
-        text='test my new command'
-        value='Hello, world!' />
+      <placeholder argument='classname'>
+        <choice limit={8}>{classlist}</choice>
+      </placeholder>
     )
   }
 }
 
-export const extensions = [MyNewCommand]
+export const ISCClassDocs = {
+  extends: [Command],
+
+  execute (result) {
+    let url = (
+      'http://docs.intersystems.com/latest/csp/documatic/%25CSP.Documatic.cls?PAGE=CLASS'+
+      '&LIBRARY='+(result.classname.substr(0,3)=="Ens"? "ENSLIB": "%25SYS")+
+      '&CLASSNAME='+result.classname
+    )
+    openURL({url: url})
+  },
+
+  describe () {
+    return (
+      <sequence>
+       <literal text='cls '/>
+       <ISCClass id='classname'/>
+      </sequence>      
+    )
+  }
+}
+
+export const extensions = [ISCClassDocs]
